56d6c7b47d8f6bbfa49c98d5e81e237c2f4fcf0c
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-09-16 12:40:23 +0200

    cri: Prepare isolators

--- rkt/app_add.go
+++ rkt/app_add.go
@@ -114,13 +114,17 @@ func runAppAdd(cmd *cobra.Command, args []string) (exit int) {
 		rktgid = -1
 	}
 
+	pcfg := stage0.PrepareConfig{
+		CommonConfig: &ccfg,
+		Apps:         &rktApps,
+	}
 	rcfg := stage0.RunConfig{
 		CommonConfig: &ccfg,
 		UseOverlay:   p.UsesOverlay(),
 		RktGid:       rktgid,
 	}
 
-	err = stage0.AddApp(rcfg, p.Path(), img)
+	err = stage0.AddApp(pcfg, rcfg, p.Path(), img)
 	if err != nil {
 		stderr.PrintE("error adding app to pod", err)
 		return 1
